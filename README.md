37941988c6ef1780ff2ee38dbbc2af68c48e3dcc
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2025-04-10 19:53:28 -0300

    Agregando archivo readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Password Generator
+Esta es una aplicación que te permite generar contraseñas aleatorias ajustando ciertos parámetros. Además, te permite validar la seguridad de las contraseñas.
+He realizado este proyecto para la asignatura de Desarrollo de Software de la carrera de Ingeniería en Sistemas de Información en la Universidad Tecnológica de San Francisco.
+
+## Despliegue
+Puedes acceder a la aplicación en línea a través de este enlace:
+[Password Generator](https://maico-zurbriggen.github.io/password_generator/)
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /*Declaramos la url base para no tener problemas al subir nuestra aplicacion a un host como GitHub Pages*/
+  base: '/password_generator/',
+  /*Nos aseguramos de que nuestro directorio dist para el despliegue reciba archivos js y no jsx*/
+  build: { 
+    rollupOptions: { 
+      output: { 
+        entryFileNames: '[name].js', 
+        chunkFileNames: '[name].js', 
+        assetFileNames: '[name][extname]', 
+      }, 
+    }, 
+  },
 })
